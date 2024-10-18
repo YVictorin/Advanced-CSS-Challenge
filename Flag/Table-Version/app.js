8e24972d69aa0ce1a56816c09297f834a3b1effd
YVictorin <vicyonjou@gmail.com>
2024-10-18 07:48:07 -0400

    refactor: use positioning for correct proportions

--- Flag/Table-Version/app.js
+++ Flag/Table-Version/app.js
@@ -1,6 +1,6 @@
 const flagContainer = document.querySelector('.flag-container');
 
-for(let i = 0; i < 23; i++) {
+for(let i = 0; i < 12; i++) {
     const td = document.createElement('tr');
     const tr = document.createElement('td');
 
